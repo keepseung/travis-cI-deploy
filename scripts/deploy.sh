da8bcc45aad9c51e4e51bb94c2c6b1aeb39f971f
keepseung <keepseung@gmail.com>
2022-05-28 22:58:31 +0900

    배포 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -26,8 +26,5 @@ echo "> $JAR_NAME에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties \
-        #-Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 

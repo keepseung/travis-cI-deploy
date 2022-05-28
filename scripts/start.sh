70b061614b37d9a22aa203bdd296cbbba9667b84
keepseung <keepseung@gmail.com>
2022-05-29 00:14:37 +0900

    무중단 배포 테스트

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,3 +30,4 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+

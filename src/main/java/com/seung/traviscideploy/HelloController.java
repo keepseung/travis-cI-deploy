ee84152531d75c2c65d3461be178c7c66c384945
keepseung <keepseung@gmail.com>
2022-05-28 23:54:52 +0900

    배포 테스트

--- src/main/java/com/seung/traviscideploy/HelloController.java
+++ src/main/java/com/seung/traviscideploy/HelloController.java
@@ -1,12 +1,34 @@
 package com.seung.traviscideploy;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class HelloController {
+    private final Environment env;
+
+    public HelloController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/")
     public String hello(){
-        return "안녕하세요~~~V3";
+        return "안녕하세요~~~V4";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real","real1","real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 }

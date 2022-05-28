bd534358f6f740132f35aefcb4b1d5bfc905168d
keepseung <keepseung@gmail.com>
2022-05-28 15:56:55 +0900

    프로젝트 초기화

---
+++ src/main/java/com/seung/traviscideploy/TravisCiDeployApplication.java
@@ -0,0 +1,13 @@
+package com.seung.traviscideploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seung/traviscideploy/TravisCiDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seung.traviscideploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
